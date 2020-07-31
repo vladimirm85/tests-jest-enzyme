3dbbeee54e42030805a2b63156bb300bb27cffa2
vladimirm85 <vladimirm85@gmail.com>
2020-07-31 20:30:08 +0300

    enzyme jest-enzyme enzyme-adapter added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
 test('renders learn react link', () => {
