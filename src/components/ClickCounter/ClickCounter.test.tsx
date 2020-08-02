48342b61c63b29cc5bc9570d4053cd0d30fe5be1
vladimirm85 <vladimirm85@gmail.com>
2020-08-02 22:31:46 +0300

    Enzyme configure to setupTests.ts

--- src/components/ClickCounter/ClickCounter.test.tsx
+++ src/components/ClickCounter/ClickCounter.test.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow, ShallowWrapper } from 'enzyme';
 import { ClickCounter, StateInterface } from './ClickCounter';
 import { findByTestAttr } from '../../../utils/testsUtils';
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 /**
  * Factory function to create ShallowWrapper for the App component
--- src/components/Jotto/Congrats/Congrats.test.tsx
+++ src/components/Jotto/Congrats/Congrats.test.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow, ShallowWrapper } from 'enzyme';
 import { Congrats, CongratsPropsInterface } from './Congrats';
 import { findByTestAttr } from '../../../../utils/testsUtils';
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const wrapperSetup = (props: CongratsPropsInterface) =>
   shallow(<Congrats {...props} />);
