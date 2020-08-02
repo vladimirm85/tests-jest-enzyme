db6022dc01c1606e08bcdefc931c2d92fcd4624d
vladimirm85 <vladimirm85@gmail.com>
2020-08-02 22:42:40 +0300

    GuessedWords Component start code

---
+++ src/components/Jotto/GuessedWords/GuessedWords.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const GuessedWords = () => <div>Guessed words</div>;
--- src/components/Jotto/Jotto.tsx
+++ src/components/Jotto/Jotto.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Congrats } from './Congrats';
+import { GuessedWords } from './GuessedWords';
 
 export const Jotto = () => (
   <div>
     <Congrats success={true} />
+    <GuessedWords />
   </div>
 );
